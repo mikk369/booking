d52bf6dcf29b82e8b808de79a723394cca73231a
mikk369 <mikk.ereline@vikk.ee>
2024-04-29 22:46:45 +0300

    replaced  woocommerce api endpoint

--- src/GetProducts.js
+++ src/GetProducts.js
@@ -2,19 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const GetProducts = () => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [products, setProduct] = useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
         // Make a GET request to the WooCommerce product endpoint
-        const response = await axios.get(`https://webcodes.ee/test/wp-json/wc/v3/products/`, {
-          params: {
-            consumer_key: 'ck_5dac46908b30df591e0f5d62185400c67972b883',
-            consumer_secret: 'cs_f8a2cb6191aeffcffb876e792b35c4a7ceeaaaca',
-          }
-        });
+        const response = await axios.get(
+          `https://webcodes.ee/test/wp-json/wp/v2/products`
+        );
+        console.log(response.data);
         // Set the retrieved product in the state
         setProduct(response.data);
         setLoading(false);
@@ -29,29 +27,34 @@ const GetProducts = () => {
 
   if (loading) {
     return (
-      <div className='loading-container'>
-        <div className='loading'>Loading...</div>
+      <div className="loading-container">
+        <div className="loading">Loading...</div>
       </div>
-    )
+    );
   }
 
   return (
-    <div className='productPage'>
-    <h2>Recent Products</h2>
-    <ul className='products'>
-      {products.map(product => (
-        <li className="product" key={product.id}>
-          <h3>{product.name}</h3>
-          <p dangerouslySetInnerHTML={{__html: product.description}}></p>
-          <img src={product.images[0].src} alt={product.name} />
-          <span dangerouslySetInnerHTML={{__html: product.short_description}}></span>
-          <p className='regularPrice'>Regular price: {product.regular_price}</p>
-          <p>Price: {product.price}</p>
-          {/* Add more product details as needed */}
-        </li>
-      ))}
-    </ul>
-  </div>
+    <div className="productPage">
+      <h2>Recent Products</h2>
+      <ul className="products">
+        {products.map((product) => (
+          <li className="product" key={product.id}>
+            <h3>{product.title.rendered}</h3>
+            <img src={product.featured_media} alt={product.title.rendered} />
+            <p
+              dangerouslySetInnerHTML={{
+                __html: product.content.rendered,
+              }}></p>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: product.short_description,
+              }}></span>
+
+            {/* Add more product details as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
