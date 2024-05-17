4f65d722eed2c4d1854e48e689bce0b2b1d91a34
mikk369 <mikk.ereline@gmail.com>
2024-05-17 11:19:55 +0300

    admin page rendered with vanilla js

--- my-custom-admin-page/my-custom-admin-page.php
+++ my-custom-admin-page/my-custom-admin-page.php
@@ -11,7 +11,7 @@ function custom_admin_page_menu() {
         'Broneeringute leht', // Page title
         'Broneeringute leht', // Menu title
         'manage_options', // Capability required
-        'Broneeringute leht', // Menu slug
+        'Broneeringute_leht', // Menu slug
         'render_custom_admin_page' // Callback function to render page content
     );
 }
@@ -19,28 +19,21 @@ add_action('admin_menu', 'custom_admin_page_menu');
 
 // Render custom admin page
 function render_custom_admin_page() {
-    echo '<div id="custom-admin-page-root">asd</div>';
+    echo '<div id="custom-admin-page-root"></div>';
 }
 
 add_action('admin_enqueue_scripts', 'enqueue_custom_admin_page_scripts');
 
 function enqueue_custom_admin_page_scripts($hook_suffix) {
-    if ($hook_suffix !== 'toplevel_page_broneeringute-leht') {
+    if ($hook_suffix !== 'toplevel_page_Broneeringute_leht') {
         return;
     }
 
     wp_enqueue_script(
         'custom-admin-page-react-app',
-        plugin_dir_url(__FILE__) . 'my-admin-page/build/static/js/main.js',
+        plugin_dir_url(__FILE__) . 'my-custom-admin-page.js',
         array('wp-element'),
-        filemtime(plugin_dir_path(__FILE__) . 'my-admin-page/build/static/js/main.js'),
+        filemtime(plugin_dir_path(__FILE__) . 'my-custom-admin-page.js'),
         true
-    );
-
-    wp_enqueue_style(
-        'custom-admin-page-react-app',
-        plugin_dir_url(__FILE__) . 'my-admin-page/build/static/css/main.css',
-        array(),
-        filemtime(plugin_dir_path(__FILE__) . 'my-admin-page/build/static/css/main.css')
     );
 }
